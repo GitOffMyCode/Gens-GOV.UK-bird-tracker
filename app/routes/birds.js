a3318ce57250f138d02af8103eab2b77f5336bc3
GitOffMyCode <52583811+GitOffMyCode@users.noreply.github.com>
2023-12-20 16:07:58 +0000

    status filters progressed

--- app/routes/birds.js
+++ app/routes/birds.js
@@ -1,12 +1,29 @@
+const _ = require("lodash");
+
 module.exports = (router) => {
+  // get birds – including any filters
   router.get("/", (req, res) => {
     let birds = req.session.data.birds;
+
+    let selectedStatusFilters = _.get(req.session.data.filters, "statuses");
+
+    // check if user has selected status filters
+    if (_.get(selectedStatusFilters, "length")) {
+      birds = birds.filter((bird) => {
+        let matchesStatus = true;
+
+        matchesStatus = selectedStatusFilters.includes(bird.status);
+
+        return matchesStatus;
+      });
+    }
 
     res.render("/index", {
       birds,
     });
   });
 
+  // get bird by id
   router.get("/:birdId", (req, res) => {
     let bird = req.session.data.birds.find(
       (bird) => bird.id === req.params.birdId
